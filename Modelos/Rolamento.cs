8b4a64d2c46ccfd1ea4e9f8ece1b951c888d4ad1
Diego Moreira Assad <diego.moreira@hotmail.com>
2018-06-11 22:06:03 -0300

    Criação Classe Rolamentos e inicio classe Controllers

---
+++ Modelos/Rolamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Rolamento
+    {
+        public int RolamentoID { get; set; }
+
+        public string Sku { get; set; }
+
+        public int Di { get; set; }
+
+        public int Do { get; set; }
+
+        public int W1 { get; set; }
+            
+        public bool Ativo { get; set; }
+    }
+}
